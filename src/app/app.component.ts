a2a28ce22fc2b9fb78b62437ade846d1122a3fa2
Anselmo <ansinftec17@gmail.com>
2023-06-17 18:53:14 -0300

    FEAT:Adicionando arquivos para adicionar tradução

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-tradutor';
+  readonly translate: TranslateService;
+  public mensagens: string = '';
+  public commentId: string = '2344';
+  public language: string = 'Português';
+  public perfil: string = 'Especialistas';
+
+
+  constructor(tlt: TranslateService) {
+    this.translate = tlt;
+  }
+
+  ngOnInit(): void {}
+
+  // metodo para enviar mensagem no serviço ja que nao tem html
+  showTranslationAlert(){
+     // adicionando tradução com variaveis
+     this.translate.get('comments').subscribe((response: string) => {
+      // nessa parte pegar o conteudo da variavel e adicionar na mensagen 
+      console.log('------------ comentarios --------------', response);
+
+      // adicionar a mensagem de alerta aqui
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'pt',
+      extend: true,
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
